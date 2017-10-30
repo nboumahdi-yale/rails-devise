59b243a5c72651c48c2b8327cc07d514ad9106a5
nboumahdi-yale <nadia.boumahdi@yale.edu>
2017-10-30 14:07:06 -0400

    Deactivate whitelist_attributes

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module RailsDevise
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
-    config.active_record.whitelist_attributes = false
+    # config.active_record.whitelist_attributes = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
